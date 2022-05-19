e5f8c215333920d9b3ed84549bf682d20ce41556
Jaroslaw Witowski <jwitowski@atlassian.com>
2022-05-19 13:34:54 +0200

    feat: show card badges

--- src/index.js
+++ src/index.js
@@ -1,40 +1,44 @@
-var WHITE_ICON =
-  "https://cdn.hyperdev.com/us-east-1%3A3d31b21c-01a0-4da2-8827-4bc6e88b7618%2Ficon-white.svg";
-var BLACK_ICON =
-  "https://cdn.hyperdev.com/us-east-1%3A3d31b21c-01a0-4da2-8827-4bc6e88b7618%2Ficon-black.svg";
-
-// This code sample uses the 'node-fetch' library:
-// https://www.npmjs.com/package/node-fetch
-
-// const boardID = await t.board('id')
-
-// const response = await fetch(`https://api.trello.com/1/boards/{id}/lists?key=${API_KEY}&token=${API_TOKEN}`, {
-//   method: 'GET',
-//   headers: {
-//     'Accept': 'application/json'
-//   }
-// })
-
-// const data = await response.text();
-// console.log(data);
+const ICON = "https://cdn-icons-png.flaticon.com/512/1983/1983632.png";
+const FIRST_POSITION_MAGIC = 65535;
 
 window.TrelloPowerUp.initialize({
   "board-buttons": function (t, opts) {
     return t.lists("all").then(function (lists) {
-      // console.log(JSON.stringify(lists, null, 2));
       const data = lists.map((list) => `${list.name} [${list.cards.length}]`);
-      const buttonLabel = data.join(' ');
-      return [{
-        // we can either provide a button that has a callback function
-        icon: {
-          dark: WHITE_ICON,
-          light: BLACK_ICON
+      const buttonLabel = data.join(" ");
+      return [
+        {
+          icon: {
+            dark: ICON,
+            light: ICON,
+          },
+          text: buttonLabel,
         },
-        text: buttonLabel,
-        // callback: () =>{},
-        // condition: 'edit'
-      }];
+      ];
     });
-
+  },
+  "card-badges": function (t, opts) {
+    return t.card("id").then(function ({ id }) {
+      t.list("cards").then(function (cards) {
+        const firstCardId = cards[0].id;
+        // const data = lists.map((list) => `${list.name} [${list.cards.length}]`);
+        // const buttonLabel = data.join(" ");
+        if (id === firstCardId) {
+          return [
+            {
+              dynamic: function () {
+                return {
+                  text:
+                    "Dynamic " + (Math.random() * 100).toFixed(0).toString(),
+                  icon: "./images/icon.svg",
+                  color: "green",
+                  refresh: 10,
+                };
+              },
+            },
+          ];
+        }
+      });
+    });
   },
 });
