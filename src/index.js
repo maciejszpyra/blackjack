b2a6cfef525c64fe9a80277e746a94f9387cfa6b
Maciej Szpyra <mszpyra@atlassian.com>
2022-05-19 12:11:38 +0200

    wrap in async

--- src/index.js
+++ src/index.js
@@ -5,11 +5,40 @@ var BLACK_ICON = 'https://cdn.hyperdev.com/us-east-1%3A3d31b21c-01a0-4da2-8827-4
 // https://www.npmjs.com/package/node-fetch
 
 // const boardID = await t.board('id')
-const lists = await t.list("all")
-
-console.log(lists);
 
 
+async function init() {
+  var t = window.TrelloPowerUp.iframe();
+  const lists = await t.list("all")
+
+  console.log(lists);
+
+  window.TrelloPowerUp.initialize({
+    'board-buttons': function (t, opts) {
+      return [{
+        // we can either provide a button that has a callback function
+        icon: {
+          dark: WHITE_ICON,
+          light: BLACK_ICON
+        },
+        text: 'Callback',
+        callback: () =>{},
+        condition: 'edit'
+      }, {
+        // or we can also have a button that is just a simple url
+        // clicking it will open a new tab at the provided url
+        icon: {
+          dark: WHITE_ICON,
+          light: BLACK_ICON
+        },
+        text: 'URL',
+        condition: 'always',
+        url: 'https://trello.com/inspiration',
+        target: 'Inspiring Boards' // optional target for above url
+      }];
+    }
+  });
+}
 // const response = await fetch(`https://api.trello.com/1/boards/{id}/lists?key=${API_KEY}&token=${API_TOKEN}`, {
 //   method: 'GET',
 //   headers: {
@@ -20,32 +49,6 @@ console.log(lists);
 // const data = await response.text();
 // console.log(data);
 
-var onBtnClick = function (t, opts) {
-  console.log('Someone clicked the button');
-};
 
-window.TrelloPowerUp.initialize({
-  'board-buttons': function (t, opts) {
-    return [{
-      // we can either provide a button that has a callback function
-      icon: {
-        dark: WHITE_ICON,
-        light: BLACK_ICON
-      },
-      text: 'Callback',
-      callback: onBtnClick,
-      condition: 'edit'
-    }, {
-      // or we can also have a button that is just a simple url
-      // clicking it will open a new tab at the provided url
-      icon: {
-        dark: WHITE_ICON,
-        light: BLACK_ICON
-      },
-      text: 'URL',
-      condition: 'always',
-      url: 'https://trello.com/inspiration',
-      target: 'Inspiring Boards' // optional target for above url
-    }];
-  }
-});
+
+init();
